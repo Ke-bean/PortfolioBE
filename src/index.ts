59a0185195347d02de1a6f1202b72b61c00c51be
kebean <chenqiua@gmail.com>
2024-03-10 23:09:12 +0200

    enabled cors

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,12 @@ const swaggerDoc = require("swagger-ui-express");
 import blogsRouter from "./routes/blogs"; 
 
 const app = express();
+app.use(cors());
 if(!config.get("jwtPrivateKey")){
     console.error("Fatal Error: JwtPrivateKey is not defined.")
     process.exit(1);
 }
 app.use(express.json())
-app.use(cors());
 app.use("/documentation", swaggerDoc.serve);
 app.use("/documentation", swaggerDoc.setup(swaggerDocumentation))
 mongoose.Promise = Promise;
